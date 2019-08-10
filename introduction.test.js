a09dcb23c27b5469f30804a9609acea1ee8236e6
Tinghui Song <tsong008@ucr.edu>
2019-08-10 15:47:46 +0100

    modified introduction

--- introduction.test.js
+++ introduction.test.js
@@ -2,4 +2,8 @@ const introduction = require ("./introduction.js")
 
 test ("introduce hannah", function(){
 	expect(introduction("hannah")).toBe("hello my name is hannah");
-})
+});
+
+test ('introduction nobody',function(){
+	expect(introduction("")).toBe("hello my name is" );
+});
